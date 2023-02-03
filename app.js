c5db26779f7a0aa2aa503a9e16d670a1065f880c
Sandeep Reddy Onteddy <sandeep@Jitendras-MacBook-Pro.local>
2023-02-03 14:54:00 -0700

    Add db and dependencies

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin:<password>@cluster0.zwtjngi.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin:admin@cluster0.zwtjngi.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
